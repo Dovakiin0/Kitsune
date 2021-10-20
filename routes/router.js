9cc421066bd50497edc0587da5a469d8c7d9b01a
Dovakiin0 <subedibs@outlook.com>
2021-10-20 22:47:05 +0545

    added cacheeing

--- routes/router.js
+++ routes/router.js
@@ -5,12 +5,16 @@ const anime = require("./anime.routes");
 const genre = require("./genre.routes");
 const schedule = require("./schedule.routes");
 
+const apiCache = require("apicache");
+
+const cache = apiCache.middleware;
+
 module.exports = (app) => {
   app.use(express.json());
   app.use(cors());
   app.use(express.urlencoded({ extended: false }));
 
-  app.use("/api/v1/anime", anime);
-  app.use("/api/v1/genre", genre);
-  app.use("/api/v1/schedule", schedule);
+  app.use("/api/v1/anime", cache("5 minutes"), anime);
+  app.use("/api/v1/genre", cache("5 minutes"), genre);
+  app.use("/api/v1/schedule", cache("5 minutes"), schedule);
 };
