dcaf7766b156c3994443b740842f6b2f0ac0b43c
dovakiin0 <subedibs@outlook.com>
2023-05-01 21:04:15 +0545

    tos and dmca added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,15 @@ import { Roboto } from "next/font/google";
 import SearchAnime from "@/partials/SearchAnime";
 import { Metadata } from "next";
 import { FaDiscord, FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 const APP_NAME = "Kitsune";
 const APP_DEFAULT_TITLE = "Kitsune | Anime Streaming";
-const APP_TITLE_TEMPLATE = "Kitsune - PWA App";
 const APP_DESCRIPTION = "Stream your favourite anime with ease and no ads";
 
 export const metadata: Metadata = {
   applicationName: APP_NAME,
-  title: {
-    default: APP_DEFAULT_TITLE,
-    template: APP_TITLE_TEMPLATE,
-  },
+  title: APP_DEFAULT_TITLE,
   description: APP_DESCRIPTION,
   appleWebApp: {
     capable: true,
@@ -29,18 +26,13 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     siteName: APP_NAME,
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
+    title: APP_DEFAULT_TITLE,
+
     description: APP_DESCRIPTION,
   },
   twitter: {
     card: "summary",
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
+    title: APP_DEFAULT_TITLE,
     description: APP_DESCRIPTION,
   },
 };
@@ -87,19 +79,6 @@ export default function RootLayout({
                 <li>
                   <a href="/waifu">Waifu</a>
                 </li>
-                <li>
-                  <a
-                    href="https://github.com/Dovakiin0/Kitsune"
-                    target="_blank"
-                  >
-                    <FaGithub size={25} />
-                  </a>
-                </li>
-                <li>
-                  <a href="https://discord.gg/6yAJ3XDHTt" target="_blank">
-                    <FaDiscord size={25} />
-                  </a>
-                </li>
               </ul>
             </div>
             <div className="flex items-center">
@@ -124,14 +103,6 @@ export default function RootLayout({
                 <a href="/waifu">Waifu</a>
               </li>
             </ul>
-            <div className="flex space-x-5 items-center ml-10">
-              <a href="https://github.com/Dovakiin0/Kitsune" target="_blank">
-                <FaGithub size={25} />
-              </a>
-              <a href="https://discord.gg/6yAJ3XDHTt" target="_blank">
-                <FaDiscord size={25} />
-              </a>
-            </div>
           </div>
           <div className="navbar-end">
             <div className="w-full lg:w-1/2 hidden lg:block mr-1">
@@ -143,6 +114,31 @@ export default function RootLayout({
           <SearchAnime />
         </div>
         {children}
+        <footer className="w-full bg-base-300 shadow-xl p-5 flex flex-col items-center space-y-5">
+          <Image src="/icon.png" alt="logo" width="100" height="100" />
+          <div className="flex space-x-5 items-center">
+            <a href="https://github.com/Dovakiin0/Kitsune" target="_blank">
+              <FaGithub size={25} />
+            </a>
+            <a href="https://discord.gg/6yAJ3XDHTt" target="_blank">
+              <FaDiscord size={25} />
+            </a>
+          </div>
+          <div className="flex space-x-5">
+            <Link href="/tos" className="text-gray-300" shallow>
+              Terms of Service
+            </Link>
+            <p>&#9671;</p>
+            <Link href="/dmca" className="text-gray-300" shallow>
+              DMCA
+            </Link>
+          </div>
+          <p className="text-sm text-gray-300">
+            Kitsune does not store any files on our server, we only linked to
+            the media which is hosted on 3rd party services.
+          </p>
+          <p className="text-sm text-gray-300">&copy; Kitsune</p>
+        </footer>
       </body>
     </html>
   );
