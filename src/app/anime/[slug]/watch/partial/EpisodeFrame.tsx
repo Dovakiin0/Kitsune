2af88968397ccdef0b60591fef8475fbbec21ce9
dovakiin0 <subedibs@outlook.com>
2023-06-05 19:38:41 +0545

    next and prev button working:

--- src/app/anime/[slug]/watch/partial/EpisodeFrame.tsx
+++ src/app/anime/[slug]/watch/partial/EpisodeFrame.tsx
@@ -11,27 +11,38 @@ type Props = {
 };
 
 function EpisodeFrame({ episode, animeInfo }: Props) {
+  const onNext = () => {
+    if (animeInfo.episodes.at(-1)?.number === episode.number) return;
+    window.location.href = `/anime/${animeInfo.slug}/watch?ep=${animeInfo.episodes[episode.number - 1 + 1]?.id
+      }`;
+  };
+
+  const onPrev = () => {
+    if (episode.number === 1) return;
+    window.location.href = `/anime/${animeInfo.slug}/watch?ep=${animeInfo.episodes[episode.number - 1 - 1]?.id
+      }`;
+  };
+
   return (
     <>
       <KitsunePlayer episodeInfo={episode} animeInfo={animeInfo} />
 
       <div className="flex justify-between items-center lg:p-5 p-2 gap-5">
-        <a
-          href={`/anime/${animeInfo.slug}/watch?ep=${animeInfo.episodes[episode.number - 1 - 1]?.id
-            }`}
-        >
-          <FaBackward size={25} className="hover:text-primary cursor-pointer" />
-        </a>
+        <FaBackward
+          size={25}
+          className="hover:text-primary cursor-pointer"
+          onClick={onPrev}
+        />
 
         <p className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-200 to-pink-400">
           Episode {episode?.number}
         </p>
-        <a
-          href={`/anime/${animeInfo.slug}/watch?ep=${animeInfo.episodes[episode.number - 1 + 1]?.id
-            }`}
-        >
-          <FaForward size={25} className="hover:text-primary cursor-pointer" />
-        </a>
+
+        <FaForward
+          size={25}
+          className="hover:text-primary cursor-pointer"
+          onClick={onNext}
+        />
       </div>
       <div className="lg:pl-5 lg:pr-5">
         <EpisodeLayout animeInfo={animeInfo} episode={episode} />
