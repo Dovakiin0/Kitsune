0042d0b8e5ef598325862d9b47c9d5e0cf0e8374
Ve Ka <57703430+Ve-Ka@users.noreply.github.com>
2023-08-02 12:52:16 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 });
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["i.waifu.pics"],
   },
+  output: 'standalone',
 };
 
-module.exports = withPWA({
-  ...nextConfig,
-  output: 'standalone',
-});
+module.exports = withPWA(nextConfig);
