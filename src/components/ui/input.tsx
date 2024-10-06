8f53d0afa38e38ec852aa6376a2a1f33eaa53f0e
Sujan <sujanbutang@gmail.com>
2024-10-06 13:16:28 +0545

    chore: fixed eslint errors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,8 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
+//eslint-disable-next-line
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
@@ -17,9 +18,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
+
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
-export let DOMAIN_URI = `${process.env.NODE_ENV === "production" ? "https" : "http"
+export const DOMAIN_URI = `${process.env.NODE_ENV === "production" ? "https" : "http"
   }://${process.env.DOMAIN_URL}`;
 
-export let BASE_URI = DOMAIN_URI + "/api";
-export let NEWS_URI = DOMAIN_URI + "/api/news/ann";
-export let WAIFU_URI = "https://api.waifu.pics";
-export let TMDB_URI = "https://api.themoviedb.org/3";
+export const BASE_URI = DOMAIN_URI + "/api";
+export const NEWS_URI = DOMAIN_URI + "/api/news/ann";
+export const WAIFU_URI = "https://api.waifu.pics";
+export const TMDB_URI = "https://api.themoviedb.org/3";
