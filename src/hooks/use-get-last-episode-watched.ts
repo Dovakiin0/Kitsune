06464e40136e3f658c8de8df1c44dc74377f7c0b
Bishal Subedi <subedibs@outlook.com>
2024-11-19 15:40:26 +0545

    feat: fix build

--- src/hooks/use-get-last-episode-watched.ts
+++ src/hooks/use-get-last-episode-watched.ts
@@ -2,21 +2,25 @@ import { useState, useEffect } from "react";
 import { IWatchedAnime } from "@/types/watched-anime";
 
 export const useGetLastEpisodeWatched = (animeId: string) => {
-    const [lastEpisodeWatched, setLastEpisodeWatched] = useState<string | null>(null);
+  const [lastEpisodeWatched, setLastEpisodeWatched] = useState<string | null>(
+    null,
+  );
 
-    useEffect(() => {
-        const watchedDetails: Array<IWatchedAnime> =
-            JSON.parse(localStorage.getItem("watched") as string) || [];
+  useEffect(() => {
+    const watchedDetails: Array<IWatchedAnime> =
+      JSON.parse(localStorage.getItem("watched") as string) || [];
 
-        const anime = watchedDetails.find((watchedAnime) => watchedAnime.anime === animeId);
+    const anime = watchedDetails.find(
+      (watchedAnime) => watchedAnime.anime.id === animeId,
+    );
 
-        if (anime && anime.episodes.length > 0) {
-            const lastWatched = anime.episodes[anime.episodes.length - 1]; // Get the last episode
-            setLastEpisodeWatched(lastWatched);
-        } else {
-            setLastEpisodeWatched(null); // No episodes watched
-        }
-    }, [animeId]);
+    if (anime && anime.episodes.length > 0) {
+      const lastWatched = anime.episodes[anime.episodes.length - 1]; // Get the last episode
+      setLastEpisodeWatched(lastWatched);
+    } else {
+      setLastEpisodeWatched(null); // No episodes watched
+    }
+  }, [animeId]);
 
-    return lastEpisodeWatched;
+  return lastEpisodeWatched;
 };
