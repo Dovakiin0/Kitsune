fd67dbf3372ebfead829f3ef66d5459d4426ff94
Bishal Subedi <subedibs@outlook.com>
2026-03-14 14:25:50 +0545

    fix: fallback to temp player

--- src/app/anime/watch/video-player-section.tsx
+++ src/app/anime/watch/video-player-section.tsx
@@ -2,36 +2,35 @@
 
 import React, { useEffect, useState } from "react";
 import { useAnimeStore } from "@/store/anime-store";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
 import { IWatchedAnime } from "@/types/watched-anime";
-const KitsunePlayer = dynamic(() => import("@/components/kitsune-player"), {
-  ssr: false,
-});
+// const KitsunePlayer = dynamic(() => import("@/components/kitsune-player"), {
+//   ssr: false,
+// });
 import { useGetEpisodeData } from "@/query/get-episode-data";
 import { useGetEpisodeServers } from "@/query/get-episode-servers";
 import { getFallbackServer } from "@/utils/fallback-server";
-import { Captions, Mic } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Switch } from "@/components/ui/switch";
+import { AlertCircleIcon } from "lucide-react";
+// import { Button } from "@/components/ui/button";
+// import { Switch } from "@/components/ui/switch";
 import { useAuthStore } from "@/store/auth-store";
-import { pb } from "@/lib/pocketbase";
-// import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-// import { useSearchParams } from "next/navigation";
+// import { pb } from "@/lib/pocketbase";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { useSearchParams } from "next/navigation";
 
 const VideoPlayerSection = () => {
-  // const searchParams = useSearchParams();
-  // const episodeId = searchParams.get("episode");
+  const searchParams = useSearchParams();
+  const episodeId = searchParams.get("episode");
   const { selectedEpisode, anime } = useAnimeStore();
 
-  const { data: serversData, isLoading: isLoadingServers } =
-    useGetEpisodeServers(selectedEpisode);
+  const { data: serversData } = useGetEpisodeServers(selectedEpisode);
 
   const [serverName, setServerName] = useState<string>("");
   const [key, setKey] = useState<string>("");
 
-  const { auth, setAuth } = useAuthStore();
-  const [autoSkip, setAutoSkip] = useState<boolean>(auth?.autoSkip || false);
+  const { auth } = useAuthStore();
+  // const [autoSkip, setAutoSkip] = useState<boolean>(auth?.autoSkip || false);
   const [watchedDetails, setWatchedDetails] = useState<Array<IWatchedAnime>>(
     [],
   );
@@ -42,44 +41,44 @@ const VideoPlayerSection = () => {
     setKey(key);
   }, [serversData]);
 
-  useEffect(() => {
-    const storedSkip = localStorage.getItem("autoSkip");
-    if (!auth && storedSkip !== null) setAutoSkip(Boolean(storedSkip));
+  // useEffect(() => {
+  //   const storedSkip = localStorage.getItem("autoSkip");
+  //   if (!auth && storedSkip !== null) setAutoSkip(Boolean(storedSkip));
+  //
+  //   try {
+  //     const stored = localStorage.getItem("watched");
+  //     setWatchedDetails(JSON.parse(stored as string) || []);
+  //   } catch {
+  //     localStorage.removeItem("watched");
+  //   }
+  // }, []);
 
-    try {
-      const stored = localStorage.getItem("watched");
-      setWatchedDetails(JSON.parse(stored as string) || []);
-    } catch {
-      localStorage.removeItem("watched");
-    }
-  }, []);
-
-  const { data: episodeData, isLoading } = useGetEpisodeData(
+  const { data: episodeData } = useGetEpisodeData(
     selectedEpisode,
     serverName,
     key,
   );
 
-  function changeServer(serverName: string, key: string) {
-    setServerName(serverName);
-    setKey(key);
-    const preference = { serverName, key };
-    localStorage.setItem("serverPreference", JSON.stringify(preference));
-  }
-
-  async function onHandleAutoSkipChange(value: boolean) {
-    setAutoSkip(value);
-    if (!auth) {
-      localStorage.setItem("autoSkip", JSON.stringify(value));
-      return;
-    }
-    const res = await pb.collection("users").update(auth.id, {
-      autoSkip: value,
-    });
-    if (res) {
-      setAuth({ ...auth, autoSkip: value });
-    }
-  }
+  // function changeServer(serverName: string, key: string) {
+  //   setServerName(serverName);
+  //   setKey(key);
+  //   const preference = { serverName, key };
+  //   localStorage.setItem("serverPreference", JSON.stringify(preference));
+  // }
+  //
+  // async function onHandleAutoSkipChange(value: boolean) {
+  //   setAutoSkip(value);
+  //   if (!auth) {
+  //     localStorage.setItem("autoSkip", JSON.stringify(value));
+  //     return;
+  //   }
+  //   const res = await pb.collection("users").update(auth.id, {
+  //     autoSkip: value,
+  //   });
+  //   if (res) {
+  //     setAuth({ ...auth, autoSkip: value });
+  //   }
+  // }
 
   useEffect(() => {
     if (auth) return;
@@ -138,106 +137,109 @@ const VideoPlayerSection = () => {
   // /**
   // Temporary fallback player for now
   // **/
-  // return episodeId && (
-  //   <>
-  //     <div
-  //       className={
-  //         "relative w-full h-auto aspect-video min-h-[20vh] sm:min-h-[30vh] md:min-h-[40vh] lg:min-h-[60vh] max-h-[500px] lg:max-h-[calc(100vh-150px)] bg-black overflow-hidden p-4"
-  //       }
-  //     >
-  //       <iframe
-  //         src={`https://megaplay.buzz/stream/s-2/${episodeId.split("?ep=")[1]}/sub`}
-  //         width="100%"
-  //         height="100%"
-  //         allowFullScreen
-  //       ></iframe>
-  //     </div>
-  //     <div className="mt-4">
-  //       <Alert variant="destructive" className="text-red-400">
-  //         <AlertTitle className="font-bold flex items-center space-x-2">
-  //           <AlertCircleIcon size="20" />
-  //           <p>Fallback Video Player Activated</p>
-  //         </AlertTitle>
-  //         <AlertDescription>
-  //           The original video source for this episode is currently unavailable.
-  //           A fallback player has been provided for your convenience. We
-  //           recommend using an ad blocker for a smoother viewing experience.
-  //         </AlertDescription>
-  //       </Alert>
-  //     </div>
-  //   </>
-  // );
-
-  if (
-    !selectedEpisode ||
-    !anime?.anime ||
-    isLoadingServers ||
-    isLoading ||
-    !serversData ||
-    !episodeData
-  )
-    return (
-      <div className="h-auto aspect-video lg:max-h-[calc(100vh-150px)] min-h-[20vh] sm:min-h-[30vh] md:min-h-[40vh] lg:min-h-[60vh] w-full animate-pulse bg-slate-700 rounded-md"></div>
-    );
-
   return (
-    <div>
-      <KitsunePlayer
-        key={episodeData.sources?.[0].url}
-        episodeInfo={episodeData}
-        serversData={serversData}
-        animeInfo={{
-          id: anime.anime.info.id,
-          title: anime.anime.info.name,
-          image: anime.anime.info.poster,
-        }}
-        subOrDub={key as "sub" | "dub"}
-        autoSkip={autoSkip}
-      />
-      <div className="flex flex-row bg-[#0f172a]  items-start justify-between w-full p-5">
-        <div>
-          <div className="flex flex-row items-center space-x-5">
-            <Captions className="text-red-300" />
-            <p className="font-bold text-sm">SUB</p>
-            {(serversData.sub ?? []).map((s, i) => (
-              <Button
-                size="sm"
-                key={i}
-                className={`uppercase font-bold ${serverName === s.serverName && key === "sub" && "bg-red-300"}`}
-                onClick={() => changeServer(s.serverName, "sub")}
-              >
-                {s.serverName}
-              </Button>
-            ))}
-          </div>
-          {!!(serversData.dub ?? []).length && (
-            <div className="flex flex-row items-center space-x-5 mt-2">
-              <Mic className="text-green-300" />
-              <p className="font-bold text-sm">DUB</p>
-              {(serversData.dub ?? []).map((s, i) => (
-                <Button
-                  size="sm"
-                  key={i}
-                  className={`uppercase font-bold ${serverName === s.serverName && key === "dub" && "bg-green-300"}`}
-                  onClick={() => changeServer(s.serverName, "dub")}
-                >
-                  {s.serverName}
-                </Button>
-              ))}
-            </div>
-          )}
+    episodeId && (
+      <>
+        <div
+          className={
+            "relative w-full h-auto aspect-video min-h-[20vh] sm:min-h-[30vh] md:min-h-[40vh] lg:min-h-[60vh] max-h-[500px] lg:max-h-[calc(100vh-150px)] bg-black overflow-hidden p-4"
+          }
+        >
+          <iframe
+            src={`https://megaplay.buzz/stream/s-2/${episodeId.split("?ep=")[1]}/sub`}
+            width="100%"
+            height="100%"
+            allowFullScreen
+          ></iframe>
         </div>
-        <div className="flex flex-row items-center space-x-2 text-sm">
-          <Switch
-            checked={autoSkip}
-            onCheckedChange={(e) => onHandleAutoSkipChange(e)}
-            id="auto-skip"
-          />
-          <p>Auto Skip</p>
+        <div className="mt-4">
+          <Alert variant="destructive" className="text-red-400">
+            <AlertTitle className="font-bold flex items-center space-x-2">
+              <AlertCircleIcon size="20" />
+              <p>Fallback Video Player Activated</p>
+            </AlertTitle>
+            <AlertDescription>
+              The original video source for this episode is currently
+              unavailable. A fallback player has been provided for your
+              convenience. We recommend using an ad blocker for a smoother
+              viewing experience.
+            </AlertDescription>
+          </Alert>
         </div>
-      </div>
-    </div>
+      </>
+    )
   );
+
+  // if (
+  //   !selectedEpisode ||
+  //   !anime?.anime ||
+  //   isLoadingServers ||
+  //   isLoading ||
+  //   !serversData ||
+  //   !episodeData
+  // )
+  //   return (
+  //     <div className="h-auto aspect-video lg:max-h-[calc(100vh-150px)] min-h-[20vh] sm:min-h-[30vh] md:min-h-[40vh] lg:min-h-[60vh] w-full animate-pulse bg-slate-700 rounded-md"></div>
+  //   );
+  //
+  // return (
+  //   <div>
+  //     <KitsunePlayer
+  //       key={episodeData.sources?.[0].url}
+  //       episodeInfo={episodeData}
+  //       serversData={serversData}
+  //       animeInfo={{
+  //         id: anime.anime.info.id,
+  //         title: anime.anime.info.name,
+  //         image: anime.anime.info.poster,
+  //       }}
+  //       subOrDub={key as "sub" | "dub"}
+  //       autoSkip={autoSkip}
+  //     />
+  //     <div className="flex flex-row bg-[#0f172a]  items-start justify-between w-full p-5">
+  //       <div>
+  //         <div className="flex flex-row items-center space-x-5">
+  //           <Captions className="text-red-300" />
+  //           <p className="font-bold text-sm">SUB</p>
+  //           {(serversData.sub ?? []).map((s, i) => (
+  //             <Button
+  //               size="sm"
+  //               key={i}
+  //               className={`uppercase font-bold ${serverName === s.serverName && key === "sub" && "bg-red-300"}`}
+  //               onClick={() => changeServer(s.serverName, "sub")}
+  //             >
+  //               {s.serverName}
+  //             </Button>
+  //           ))}
+  //         </div>
+  //         {!!(serversData.dub ?? []).length && (
+  //           <div className="flex flex-row items-center space-x-5 mt-2">
+  //             <Mic className="text-green-300" />
+  //             <p className="font-bold text-sm">DUB</p>
+  //             {(serversData.dub ?? []).map((s, i) => (
+  //               <Button
+  //                 size="sm"
+  //                 key={i}
+  //                 className={`uppercase font-bold ${serverName === s.serverName && key === "dub" && "bg-green-300"}`}
+  //                 onClick={() => changeServer(s.serverName, "dub")}
+  //               >
+  //                 {s.serverName}
+  //               </Button>
+  //             ))}
+  //           </div>
+  //         )}
+  //       </div>
+  //       <div className="flex flex-row items-center space-x-2 text-sm">
+  //         <Switch
+  //           checked={autoSkip}
+  //           onCheckedChange={(e) => onHandleAutoSkipChange(e)}
+  //           id="auto-skip"
+  //         />
+  //         <p>Auto Skip</p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default VideoPlayerSection;
